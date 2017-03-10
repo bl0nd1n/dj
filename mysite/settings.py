806137e7fae187046228f6d4062e0d4fce6b6c16
Your Name <you@example.com>
2017-03-10 10:07:37 +0000

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i1=$fo$nd0qmx95q9cz_$#90kzak59p@73)^(fv*bw61o(b5^2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
 
 # Application definition
